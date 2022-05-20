2480ecdbd9efd3dba675fc9f26ef54051b6d71ef
name <name@gmail.com>
2022-05-20 23:21:40 +0200

    first commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -52,6 +52,7 @@ const Navbar = () => {
                    <i className="fa fa-user-plus me-1"></i> Register</NavLink>
                 <NavLink to="/cart" className="btn btn-outline-dark ms-2">
                    <i className="fa fa-shopping-cart me-1"></i> Cart ({state.length})</NavLink>
+                   
             </div>
           </div>
         </div>
